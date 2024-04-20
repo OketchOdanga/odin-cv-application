316aba9ba5581ed28d4484809c27ddb045bc7bdc
Austine Odanga <austinoketches@gmail.com>
2024-04-20 23:43:30 +0300

    feat: added education component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import PersonalInfo from './PersonalInfo'
+import Education from './Education'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <PersonalInfo/>
+    <Education/>
   </React.StrictMode>,
 )
