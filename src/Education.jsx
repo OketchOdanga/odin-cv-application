e5ff50df9894f39ad0832904b4b548460064bf70
Austine Odanga <austinoketches@gmail.com>
2024-04-20 23:44:06 +0300

    feat: created education component

--- src/Education.jsx
+++ src/Education.jsx
@@ -1,1 +1,49 @@
 /* this section will include educational experience (school name title of study and date of study) */
+
+import { useState } from "react";
+
+export default function Education() {
+    const[school, setSchool]= useState('');
+    const[course, setCourse]= useState('');
+    const[start, setStart]= useState('');
+    const[finish, setFinish]= useState('');
+
+    function handleSchool(e){ setSchool(e.target.value)}
+    function handleCourse(e){setCourse(e.target.value)}
+    function handleStart(e){setStart(e.target.value)}
+    function handleFinish(e){setFinish(e.target.value)}
+
+    function handleSubmit(e){
+        e.preventDefault();
+        setSchool(e.target.value);
+        setCourse(e.target.value);
+        setStart(e.target.value);
+        setFinish(e.target.value);
+    }
+    function handleReset(){
+        setSchool('');
+        setCourse('');
+        setStart('');
+        setFinish('');
+    }
+
+    return (
+        <>
+        <form>
+            <label htmlFor="school">School</label>
+            <input type="text" name="school" id="school" value={school} onChange={handleSchool}/>
+            <label htmlFor="course">Course</label>
+            <input type="text" name="course" id="course" value={course} onChange={handleCourse} />
+            <label htmlFor="start">Start</label>
+            <input type="date" name="start" id="start" value={start} onChange={handleStart} />
+            <label htmlFor="finish">Finish</label>
+            <input type="date" name="finish" id="finish" value={start} onChange={handleFinish} />
+            <button type="submit" onClick={handleSubmit} >Submit</button>
+            <button type="reset" onClick={handleReset}>Reset</button>
+        </form>
+        <p>{school}</p>
+        <p>{course}</p>
+        <p>{start} to {finish}</p>
+        </>
+    )
+}
