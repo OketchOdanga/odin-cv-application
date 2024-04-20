8be6d9d70a0e54a5ba421eed378cafda87e8d356
Austine Odanga <austinoketches@gmail.com>
2024-04-21 00:42:36 +0300

    feat : added work experience copmonent

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import PersonalInfo from './PersonalInfo'
 import Education from './Education'
 import './index.css'
+import WorkExperience from './WorkExperience'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <PersonalInfo/>
     <Education/>
+    <WorkExperience/>
   </React.StrictMode>,
 )
