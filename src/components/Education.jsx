b7ab8d2e4966246dd3b1f6b12d904be634bcc7e2
Austine Odanga <austinoketches@gmail.com>
2024-04-23 00:44:04 +0300

    refactored the components

---
+++ src/components/Education.jsx
@@ -0,0 +1,63 @@
+/* this section will include educational experience (school name title of study and date of study) */
+
+import { useState } from "react";
+import './styles/Education.css'
+
+export default function Education() {
+    const[education,setEducation] = useState([{}]);
+
+    function handleChange(e){
+        const {name, value} = e.target;
+        setEducation({...education, [name]: value})
+    }
+    
+    function handleReset() {
+        setEducation('')
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+    }
+
+    return (
+        <section className="hero2">
+        <form className="form-education" id="formEducation" onSubmit={handleSubmit}>
+            <div className="school">
+                <label htmlFor="school">School</label>
+                <input type="text" name="school" id="school" value={education.school} onChange={handleChange}/>
+            </div>
+
+            <div className="course">
+                <label htmlFor="course">Course</label>
+                <input type="text" name="course" id="course" value={education.course} onChange={handleChange} />
+            </div>
+
+            <div className="start">
+                <label htmlFor="start">Start</label>
+                <input type="date" name="start" id="start" value={education.start} onChange={handleChange} />
+            </div>
+
+            <div className="finish">
+                <label htmlFor="finish">Finish</label>
+                <input type="date" name="finish" id="finish" value={education.finish} onChange={handleChange} />
+            </div>
+
+            <div className="buttons">
+                <button type="submit">Add</button>
+                <button type="reset" onClick={handleReset}>Reset</button>
+            </div>
+        </form>
+
+        <div className="education">
+            <h2>Education</h2>
+            <p><strong>{education.school}</strong> <em>{education.start} to {education.finish}</em> </p>
+            <p>{education.course}</p>
+
+        </div>
+
+
+        
+        
+        </section>
+    )
+}
---
+++ src/components/WorkExperience.jsx
@@ -0,0 +1,79 @@
+/* this section adds practical/work experience (company name , position title, main responsibilities of your jobs date from and until when you worked for that company) */
+
+import { useState } from "react"
+import './styles/WorkExperience.css'
+
+export default function WorkExperience() {
+    const [work, setWork] = useState({company:'audi', position:'manager', responsibilities:['sleep'],start: '2004-4-6', finish: '2012-5-7'})
+    const [newindex,setNewIndex] = useState(null);
+    function handleChange(e) {
+        const {name, value} = e.target
+        setWork({...work, [name]: value})
+    }
+
+    function handleAddWork() {
+        if (newindex !== null) {
+            const newWork = [...work]
+            newWork[newindex] = work;
+            setWork(newWork);
+            setNewIndex(null)
+
+        } else {
+            setWork({...work, work})
+        }
+       setWork({company:'audi', position:'manager', responsibilities:['sleep'],start: '2004-4-6', finish: '2012-5-7'})
+
+    }
+
+    function handleReset() {
+        setWork('')
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+    }
+
+
+    return(
+        <section className="hero3">
+        <form className="form-work" id="formWork"  onSubmit={handleAddWork}>
+            <div className="company">
+                <label htmlFor="company">Company</label>
+                <input type="text" name="company" id="company" value={work.company} onChange={handleChange} />
+            </div>
+
+            <div className="position">
+                <label htmlFor="position">Position</label>
+                <input type="text" name="position" id="position" value={work.position} onChange={handleChange} />
+            </div>
+
+            <div className="responsibilities">
+                <label htmlFor="responsibilities">Responsibilities</label>
+                <input type="text" name="responsibilites" id="responsibilities" value={work.responsibilities} onChange={handleChange} />
+            </div>
+
+            <div className="start">
+                <label htmlFor="start">Start</label>
+                <input type="date" name="start" id="start" value={work.start} onChange={handleChange} />
+            </div>
+
+            <div className="finish">
+                <label htmlFor="finish">Finish</label>
+                <input type="date" name="finish" id="finish" value={work.finish} onChange={handleChange}/>
+            </div>
+            <div className="buttons">
+                <button type="submit" onClick={handleSubmit}>Add</button>
+                 <button type="reset" onClick={handleReset}>Reset</button>
+            </div>
+        </form>
+        <div className="work-experience">
+            <h2>Experience</h2>
+            <p><strong>{work.position}</strong> </p>
+            <p>{work.company}</p>
+            <p> <em>{work.start} to {work.finish}</em></p>
+            <p>{work.responsibilities}</p>
+            
+        </div>
+        </section>
+    )
+}
