544ae820f8edea274a41f94f3b0865639fd47105
Austine Odanga <austinoketches@gmail.com>
2024-04-26 13:47:03 +0300

    properties update

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,29 +1,34 @@
 export const CV = {
-    personalInfo:{
-    name: 'Jane Doe',
-    email: 'hey@gmail.com',
-    number: '071234568',
+    personalInfo:[{
+        id: Math.random() * 17,
+        name: 'Jane Doe',
+        email: 'hey@gmail.com',
+        number: '071234568'
     
-    },
+    }],
     Education: [{
+        id: Math.random() * 7,
         school: 'webster',
         course: 'Bsc. Engineering',
         date: 'September 2001 - December 2018'
     },{
+        id: Math.random() * 7,
         school: 'Odin',
         course: 'Bsc. Conflits',
         date: 'September 2090 - present'
     }],
     WorkExperience:[{
+        id: Math.random() * 13,
         company: 'Ting Ting',
         position: 'Manger',
-        responsibilities:['sleeping','eating'],
+        responsibilities:'sleeping, eating',
         date: 'July 2017 - December 2020'
     },
     {
+        id: Math.random() * 13,
         company: 'Wingz',
         position: 'Researcher',
-        responsibilities:['sleeping','eating'],
+        responsibilities:'sleeping ,eating',
         date: 'July 2021 - present'
     }
     ]
